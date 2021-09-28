060619933adabe3362a447e1640b39ac005b375c
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-09-27 22:57:18 -0300

    Dia 1

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const cs = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item, index)=>{
 let pizzaItem = c('.models .pizza-item').cloneNode(true); //Pega tudo que está dentro do item
 
+//Identificar a  pizza
+pizzaItem.setAttribute('data-key', index);
+
 //Imagem da pizza
 pizzaItem.querySelector('.pizza-item--img img').src = item.img;
 
@@ -19,6 +22,15 @@ pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 //Eventos de click para não atualizar a página
 pizzaItem.querySelector('a').addEventListener('click', (e)=>{
 e.preventDefault();
+
+//Indicar para o modal a pizza que esta clicada
+let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+//Informações da pizza no Modal
+c('.pizzaBig img').src = pizzaJson[key].img;
+c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
 //Ativar o modal com o click
 c('.pizzaWindowArea').style.opacity = 0;
 c('.pizzaWindowArea').style.display='flex';
